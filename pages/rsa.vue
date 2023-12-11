86f5121af2b3190ff5c0b6a78384c8e6ff8ad90d
bat <ZoeOosting@outlook.com>
2023-12-11 19:02:00 +0100

    awawaw

--- pages/rsa.vue
+++ pages/rsa.vue
@@ -15,35 +15,54 @@
         <div class="flex gap-4">
             <div class="flex flex-col gap-2">
                 <span class="font-bold">p=</span>
-                <input class="input" type="number" v-model="p">
-                <span v-if="isPrime(p)">{{ p }} is prime</span>
-                <span class="bg-warning p-2 text-warning-content rounded-lg" v-else>{{ p }} is not prime</span>
+                <select class="select" v-model="p">
+                    <option v-for="num in smallPrimes" :value="num">
+                        {{  num }}
+                    </option>
+                </select>
+                <span class="bg-warning p-2 text-warning-content rounded-lg" v-if="!isPrime(p)">{{ p }} is not prime</span>
+                <button class="btn" @click="p = randomSmallPrime()">
+                    <ArrowPathIcon class="w-6 h-6" />
+                    <span>reroll</span>
+                </button>
             </div>
             <div class="flex flex-col gap-2">
                 <span class="font-bold">q=</span>
-                <input class="input" type="number" v-model="q">
-                <span v-if="isPrime(q)">{{ q }} is prime</span>
-                <span class="bg-warning p-2 text-warning-content rounded-lg" v-else>{{ q }} is not prime</span>
+                <select class="select" v-model="q">
+                    <option v-for="num in smallPrimes" :value="num">
+                        {{  num }}
+                    </option>
+                </select>
+                <span class="bg-warning p-2 text-warning-content rounded-lg" v-if="!isPrime(q)">{{ q }} is not prime</span>
+                <button class="btn" @click="q = randomSmallPrime()">
+                    <ArrowPathIcon class="w-6 h-6" />
+                    <span>reroll</span>
+                </button>
             </div>
         </div>
+        <span v-if="q === p">p and q should not be the same</span>
         <p>
             next we will need to calculate their product n = p * q = {{ p }} * {{ q }} = {{ n }}
             and the Euler totient φ(n) = {{ phi }}
         </p>
         <h3>public key</h3>
         <p>
-            the public key must fulfill a few conditions:
+            the public key must fulfill a few conditions
         <ul>
             <li>must be prime</li>
-            
-            <li>must be less than φ(n)</li>
+            <li>must be smaller φ(n)</li>
             <li>must not be a factor φ(n)</li>
         </ul>
+        a number where this is true is: {{ chosenPublicKey }}
         </p>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ArrowPathIcon } from '@heroicons/vue/24/solid';
+useHead({
+    title: "the math behind rsa"
+})
 
 function isPrime(num: number): boolean {
     for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
@@ -52,8 +71,21 @@ function isPrime(num: number): boolean {
     return num > 1;
 }
 
-const p = ref(79)
-const q = ref(19)
+function findPrimesBelow(num: number): Array<number> {
+    return Array.from(Array(num).keys()).filter((n) => {return isPrime(n)})
+}
+
+const smallPrimes = findPrimesBelow(100).filter((num) => {return num > 3})
+const primes = findPrimesBelow(8633)
+
+function randomSmallPrime(): number {
+    const randomNumber = Math.round(Math.random() * smallPrimes.length)
+    return smallPrimes.filter((item) => {return item !== p.value && item !== q.value})[randomNumber % smallPrimes.length]
+}
+
+const p = ref(13)
+const q = ref(43)
+const publicKeyIndex = ref(50)
 
 const n = computed(() => {
     return p.value * q.value
@@ -61,5 +93,13 @@ const n = computed(() => {
 
 const phi = computed(() => {
     return (p.value - 1) * (q.value - 1)
+})
+
+const validPublicKeys = computed(() => {
+    return primes.filter((num) => {return num < phi.value}).filter((key) => {return phi.value % key !== 0})
+})
+
+const chosenPublicKey = computed(() => {
+    return validPublicKeys.value[publicKeyIndex.value % validPublicKeys.value.length]
 })
 </script>
