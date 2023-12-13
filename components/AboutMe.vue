59b8e29247b3b942e3c52ddb45b6fda0ea68d092
zoe <ZoeOosting@outlook.com>
2023-12-13 14:22:55 +0100

    center title text

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen h-full flex justify-center items-center">
+    <div class="min-h-screen h-full w-full text-center flex justify-center items-center">
         <p class="text-7xl font-rammetto">hi! i'm zoe</p>
     </div>
 </template>
