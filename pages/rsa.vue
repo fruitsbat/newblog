6856058c97b38db5a56eadb883626f40e3ee2c59
bat <ZoeOosting@outlook.com>
2023-12-22 14:23:40 +0100

    e

--- pages/rsa.vue
+++ pages/rsa.vue
@@ -1,70 +1,73 @@
 <template>
-    <div class="flex gap-4 p-4 items-center">
-        <HomeButton />
-        <span class="items-center font-black text-4xl">rsa calculator</span>
-    </div>
-    <div class="prose p-8">
-
-        <h2>generate keys</h2>
-        <p>the first step is picking two prime numbers:</p>
-        <div class="flex gap-4">
-            <div class="flex flex-col gap-2">
-                <span class="font-bold">p=</span>
-                <select class="select" v-model="p">
-                    <option v-for="num in smallPrimes" :value="num">
-                        {{ num }}
-                    </option>
-                </select>
-                <span class="bg-warning p-2 text-warning-content rounded-lg" v-if="!isPrime(p)">{{ p }} is not prime</span>
-            </div>
-            <div class="flex flex-col gap-2">
-                <span class="font-bold">q=</span>
-                <select class="select" v-model="q">
-                    <option v-for="num in smallPrimes" :value="num">
-                        {{ num }}
-                    </option>
-                </select>
-                <span class="bg-warning p-2 text-warning-content rounded-lg" v-if="!isPrime(q)">{{ q }} is not prime</span>
-            </div>
+    <div>
+        <div class="flex gap-4 p-4 items-center">
+            <HomeButton />
+            <span class="items-center font-black text-4xl">rsa calculator</span>
         </div>
-        <span v-if="q === p">p and q should not be the same</span>
-        <p>
-            next we will need to calculate their product n = p * q = {{ p }} * {{ q }} = {{ n }}
-            and the Euler totient φ({{ n }}) = {{ phi }}
-        </p>
-        <h3>public key</h3>
-        <p>
-            the public key must fulfill a few conditions
-        <ul>
-            <li>must be prime</li>
-            <li>must be smaller than φ(n) = {{ phi }}</li>
-            <li>must not be a factor φ(n) = {{ phi }}</li>
-        </ul>
-        a number where this is true is:
-        <select class="select" v-model="publicKeyIndex">
-            <option v-for="key, index in validPublicKeys" :value="index">{{ key }}</option>
-        </select>.
-        we will use it in our public key: {{ `(${chosenPublicKey}, ${n})` }}.
-        </p>
-        <h3>private key</h3>
-        <p>
-            The private key will need to be the multiplicative modular inverse of our public key and φ(n).
-            We can use the extended euclidean algorithm for this.
-        <ol>
-            <li v-for="step in euclidStepOne">
-                {{ step.solution }} = {{ step.factor }} * {{ step.value }} + {{ step.added }}
-            </li>
-        </ol>
-        <ol>
-            <li v-for="step in extendedEuclideanAlgorithm">
-                {{ `q=${step.q} r=${step.r} s=${step.s} t=${step.t}` }}
-            </li>
-        </ol>
-        if our candidate is below zero we have to add our φ(n), the modulus, to it until it is positive.
-        {{ phi }} * {{ privateKey.belowZeroCount }} + {{ extendedEuclideanAlgorithm.at(-1)?.oldT }} = {{ privateKey.key }}
-        this means that we can use {{ `(${privateKey.key}, ${n})` }} as a private key.
-        </p>
-        <!-- <h2>encoding the message</h2>
+        <div class="prose p-8">
+            <h2>generate keys</h2>
+            <p>the first step is picking two prime numbers:</p>
+            <div class="flex gap-4">
+                <div class="flex flex-col gap-2">
+                    <span class="font-bold">p=</span>
+                    <select class="select" v-model="p">
+                        <option v-for="num in smallPrimes" :value="num">
+                            {{ num }}
+                        </option>
+                    </select>
+                    <span class="bg-warning p-2 text-warning-content rounded-lg" v-if="!isPrime(p)">{{ p }} is not
+                        prime</span>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <span class="font-bold">q=</span>
+                    <select class="select" v-model="q">
+                        <option v-for="num in smallPrimes" :value="num">
+                            {{ num }}
+                        </option>
+                    </select>
+                    <span class="bg-warning p-2 text-warning-content rounded-lg" v-if="!isPrime(q)">{{ q }} is not
+                        prime</span>
+                </div>
+            </div>
+            <span v-if="q === p">p and q should not be the same</span>
+            <p>
+                next we will need to calculate their product n = p * q = {{ p }} * {{ q }} = {{ n }}
+                and the Euler totient φ({{ n }}) = {{ phi }}
+            </p>
+            <h3>public key</h3>
+            <p>
+                the public key must fulfill a few conditions
+            <ul>
+                <li>must be prime</li>
+                <li>must be smaller than φ(n) = {{ phi }}</li>
+                <li>must not be a factor φ(n) = {{ phi }}</li>
+            </ul>
+            a number where this is true is:
+            <select class="select" v-model="publicKeyIndex">
+                <option v-for="key, index in validPublicKeys" :value="index">{{ key }}</option>
+            </select>.
+            we will use it in our public key: {{ `(${chosenPublicKey}, ${n})` }}.
+            </p>
+            <h3>private key</h3>
+            <p>
+                The private key will need to be the multiplicative modular inverse of our public key and φ(n).
+                We can use the extended euclidean algorithm for this.
+            <ol>
+                <li v-for="step in euclidStepOne">
+                    {{ step.solution }} = {{ step.factor }} * {{ step.value }} + {{ step.added }}
+                </li>
+            </ol>
+            <ol>
+                <li v-for="step in extendedEuclideanAlgorithm">
+                    {{ `q=${step.q} r=${step.r} s=${step.s} t=${step.t}` }}
+                </li>
+            </ol>
+            if our candidate is below zero we have to add our φ(n), the modulus, to it until it is positive.
+            {{ phi }} * {{ privateKey.belowZeroCount }} + {{ extendedEuclideanAlgorithm.at(-1)?.oldT }} = {{ privateKey.key
+            }}
+            this means that we can use {{ `(${privateKey.key}, ${n})` }} as a private key.
+            </p>
+            <!-- <h2>encoding the message</h2>
         <p>
             we can encode our message <input class="input" type="number" v-model="message" /> using either of the keys
             usually this is done with public key.
@@ -74,6 +77,7 @@
             we can also decrypt it using our private key:
             {{ encryptedMessage }}<sup>{{ privateKey.key }}</sup> mod {{ n }} = {{ decryptedMessage }}.
         </p> -->
+        </div>
     </div>
 </template>
 
