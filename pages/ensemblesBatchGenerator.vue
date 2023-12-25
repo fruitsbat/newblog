04b90e4eb3405231a0c6c51666052da0b1078f77
bat <ZoeOosting@outlook.com>
2023-12-25 18:45:14 +0100

    hehe

--- pages/ensemblesBatchGenerator.vue
+++ pages/ensemblesBatchGenerator.vue
@@ -1,12 +1,30 @@
 <template>
-    <div>
-        <HeadingWithHomeButton title="batch script generator for numio ensembles" />
-        hehe
-    </div>
+  <div class="prose">
+    <HeadingWithHomeButton title="batch script generator for numio ensembles" />
+    <pre>
+#!/bin/bash
+	
+#SBATCH --time=1
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=5
+#SBATCH --ntasks=10
+#SBATCH --partition=west
+# Output goes to "job.out", error messages to "job.err".
+#SBATCH --output=job.out
+#SBATCH --error=job.err
+
+mpiexec python src/main.py
+</pre
+    >
+  </div>
 </template>
 
 <script setup lang="ts">
 useHead({
-    title: "numio ensembles batch script generator"
-})
+  title: "numio ensembles batch script generator",
+});
+
+definePageMeta({
+  layout: "blogpost",
+});
 </script>
