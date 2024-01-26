696c2dd756b2452c2c3a8d590111fc34e5627da8
zoe <ZoeOosting@outlook.com>
2024-01-26 16:23:06 +0100

    add strawberry

--- content/list.ts
+++ content/list.ts
@@ -1,5 +1,19 @@
 import { type Content, Page, Music, Sticker } from "./classes";
 export const contentList: Array<Content> = [
+  new Sticker({
+    created: new Date(2024, 1, 25),
+    image: {
+      description: "low poly image of a strawberry the leaves have some funky patterns on them",
+      link: "/image/strawberry.webp",
+    },
+    link: "/image/strawberry.webp",
+    title: "low poly strawberry",
+    enclosure: {
+      url: "https://zoe.kittycat.homes/image/strawberry.webp",
+      type: "image/webp",
+      title: "low poly strawberry",
+    },
+  }),
   new Page({
     title: "starchart",
     summary: "an art project that lets you generate planets for everyone",
