a00e29cdf35ee3e04688847b658d2a224b2bc1bd
zoe <zoe@kittycat.homes>
2024-02-10 14:22:59 +0100

    add some stripeys

--- components/HomeHeading.vue
+++ components/HomeHeading.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-full flex justify-center items-center">
+  <div class="w-full h-full flex justify-center items-center retro-stripes">
     <img class="w-2/5 h-auto" src="/favicon.svg" />
   </div>
 </template>
