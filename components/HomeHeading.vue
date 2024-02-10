f00a0a391e9f3e86f5ba415c0a9a9b332978332c
zoe <zoe@kittycat.homes>
2024-02-10 10:28:42 +0100

    add emoji to website

---
+++ components/HomeHeading.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full h-full flex justify-center items-center">
+    <img class="w-2/5 h-auto" src="/favicon.svg" />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
 <template>
-    <div>
-        <div class="grid xl:grid-cols-5 gap-4 p-8">
-            <AboutPanel class="xl:col-span-2" />
-            <ContentNavigation class="xl:col-span-3" />
-        </div>
+  <div>
+    <HomeHeading class="min-h-screen" />
+    <div class="grid xl:grid-cols-5 gap-4 p-8">
+      <AboutPanel class="xl:col-span-2" />
+      <ContentNavigation class="xl:col-span-3" />
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import ContentNavigation from '~/components/ContentNavigation.vue';
+import ContentNavigation from "~/components/ContentNavigation.vue";
 useHead({
-    title: "zoe website"
-})
+  title: "zoe website",
+});
 </script>
