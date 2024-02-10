89d3c77318c0fe57f7bc77337a60c9cb4061a1a8
zoe <zoe@kittycat.homes>
2024-02-10 15:02:58 +0100

    add alt text to home page

--- components/HomeHeading.vue
+++ components/HomeHeading.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="w-full h-full flex justify-center items-center retro-stripes">
-    <img class="w-2/5 h-auto" src="/favicon.svg" />
+    <img class="w-2/5 h-auto" src="/favicon.svg" alt="a cartoon picture of a bat"/>
   </div>
 </template>
