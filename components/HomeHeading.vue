1b3e35d55ead73aff63dec0cb001128de7fe9bef
zoe <zoe@kittycat.homes>
2024-02-11 10:31:29 +0100

    meow

--- components/HomeHeading.vue
+++ components/HomeHeading.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-full flex justify-center items-center retro-stripes">
-    <img class="w-2/5 h-auto" src="/favicon.svg" alt="a cartoon picture of a bat"/>
+  <div class="w-full min-h-[512px] h-full flex justify-center items-center retro-stripes">
+    <img class="w-2/3 h-auto max-h-screen" src="/favicon.svg" alt="a cartoon picture of a bat"/>
   </div>
 </template>
