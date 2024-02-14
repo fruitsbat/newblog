9342152258f4bf6a5cf300ca017c372c280e05d4
bat <zoe@kittycat.homes>
2024-02-14 14:03:37 +0100

    sdfsf

--- components/ContentList.vue
+++ components/ContentList.vue
@@ -1,32 +1,37 @@
 <template>
-    <TransitionGroup name="list" tag="ul"
-        class="grid gap-4 rounded-3xl flex-grow grid-cols-1 @2xl:grid-cols-3 @6xl:grid-cols-5 transition-all">
-        <li class="h-full" v-for="(item, index) in shownItems" :key="item.id">
-            <ContentBlogpostCard v-if="(item instanceof Page)" :post="item" />
-            <ContentMusicCard v-else-if="(item instanceof Music)" :music="item" />
-            <ContentStickerCard v-else-if="(item instanceof Sticker)" :sticker="item" />
-        </li>
+  <div class="@container">
+    <TransitionGroup
+      name="list"
+      tag="ul"
+      class="flex-grow columns-1 transition-all @md:columns-2 @lg:columns-3 @7xl:columns-5"
+    >
+      <li v-for="(item, index) in shownItems" :key="item.id" class="break-inside-avoid py-2">
+        <ContentBlogpostCard v-if="item instanceof Page" :post="item" />
+        <ContentMusicCard v-else-if="item instanceof Music" :music="item" />
+        <ContentStickerCard v-else-if="item instanceof Sticker" :sticker="item" />
+      </li>
     </TransitionGroup>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { Page, Content, Music, Sticker } from '../content/classes';
+import { Page, Content, Music, Sticker } from '../content/classes'
 
 defineProps({
-    shownItems: {
-        type: Array<Content>,
-        required: true,
-    }
+  shownItems: {
+    type: Array<Content>,
+    required: true
+  }
 })
 </script>
 
 <style>
 .list-enter-active {
-    transition: all 0.48s cubic-bezier(0.075, 0.82, 0.165, 1);
+  transition: all 0.48s cubic-bezier(0.075, 0.82, 0.165, 1);
 }
 
 .list-enter-from {
-    opacity: 0;
-    transform: scale(1.24);
+  opacity: 0;
+  transform: scale(1.24);
 }
 </style>
