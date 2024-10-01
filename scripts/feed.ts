5327dca9f052b26d1e6658b2fa2ad86a6ea404fd
zoe <zoe@kittycat.homes>
2024-10-01 11:23:23 +0200

    put image thing

--- scripts/feed.ts
+++ scripts/feed.ts
@@ -54,5 +54,13 @@ export async function getFeed(): Promise<Feed> {
       }),
     });
   }
+  for (const doc of content) {
+    let content = "";
+    if (doc.image != undefined) {
+      content =
+        content +
+        `<img src='https://zoe.kittycat.homes${doc.image}' alt='${doc.image_description}' />`;
+    }
+  }
   return feed;
 }
