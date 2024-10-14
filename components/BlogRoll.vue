8c83fcb4c534fb1f6aa5ff97cb67946cff80eeaa
bat <zoe@kittycat.homes>
2024-10-14 21:18:06 +0200

    fix the buttons

--- components/BlogRoll.vue
+++ components/BlogRoll.vue
@@ -1,12 +1,12 @@
 <template>
-  <ul class="flex flex-wrap gap-2 p-4">
-    <li v-for="button in buttons">
-      <a :href="button.linksTo" class="">
+  <ul class="flex w-full flex-wrap gap-2 p-4">
+    <li v-for="button in buttons" class="flex-grow">
+      <a :href="button.linksTo" class="h-auto w-full">
         <img
           :src="`/image/button/${button.imgUrl}`"
           loading="lazy"
           style="image-rendering: pixelated"
-          class="h-[62px] w-[176px] hover:scale-150"
+          class="min-h-[32px] min-w-[176px] h-full w-full hover:scale-150"
         />
       </a>
     </li>
@@ -16,8 +16,9 @@
 <script setup lang="ts">
 const buttons: Array<{ linksTo: string; imgUrl: string }> = [
   { linksTo: 'https://tiger.kittycat.homes', imgUrl: 'tiger.gif' },
-  { linksTo: 'https://meltknuckles.net/links', imgUrl: 'meltknuckles.gif' },
+  { linksTo: 'https://www.5snb.club/', imgUrl: '522.png' },
   { linksTo: 'https://erysdren.me', imgUrl: 'erysdren.png' },
-  { linksTo: 'https://le.alphamethyl.barr0w.net/~elysia/', imgUrl: "elysia.png"}
+  { linksTo: 'https://meltknuckles.net/links', imgUrl: 'meltknuckles.gif' },
+  { linksTo: 'https://le.alphamethyl.barr0w.net/~elysia/', imgUrl: 'elysia.png' }
 ]
 </script>
